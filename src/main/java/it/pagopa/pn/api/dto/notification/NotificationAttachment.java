be60844439161b13c88e4c0cd18e8094ef32c225
Simone <SimoneLubrano94@gmail.com>
2021-09-13 18:08:03 +0200

    Added annotations for validation

--- src/main/java/it/pagopa/pn/api/dto/notification/NotificationAttachment.java
+++ src/main/java/it/pagopa/pn/api/dto/notification/NotificationAttachment.java
@@ -8,6 +8,7 @@ import lombok.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,6 +32,7 @@ public class NotificationAttachment {
     @Schema( description = "corpo dell'allegato" )
     @JsonView(value = { NotificationJsonViews.New.class })
     @NotBlank(groups = { NotificationJsonViews.New.class })
+    @Pattern(regexp = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$", message = "The string is not in Base64", groups = { NotificationJsonViews.New.class } )
     private String body;
 
     @JsonIgnore
@@ -47,6 +49,7 @@ public class NotificationAttachment {
         @Schema( description = "Digest \"sha256\" della codifica binaria dell'allegato" )
         @JsonView(value = { NotificationJsonViews.New.class, NotificationJsonViews.Sent.class, NotificationJsonViews.Received.class })
         @NotBlank(groups = { NotificationJsonViews.New.class })
+        @Pattern(regexp = "^[A-Fa-f0-9]{64}$", message = "The string is not in Sha256", groups = { NotificationJsonViews.New.class } )
         private String sha256;
     }
 }
