5d441cc6f3756c2e85d61f563bd5ffe33ca9d805
alfonsogiustini <a.giustini@sirti.it>
2021-09-22 18:40:01 +0200

    implementazione servizio lista legalfacts

--- src/main/java/it/pagopa/pn/api/rest/PnDeliveryRestApi_methodGetSentNotificationLegalFacts.java
+++ src/main/java/it/pagopa/pn/api/rest/PnDeliveryRestApi_methodGetSentNotificationLegalFacts.java
@@ -1,16 +1,18 @@
 package it.pagopa.pn.api.rest;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import it.pagopa.pn.api.dto.notification.Notification;
+import it.pagopa.pn.api.dto.LegalFactsRow;
 
 public interface PnDeliveryRestApi_methodGetSentNotificationLegalFacts {
 
 	@GetMapping(PnDeliveryRestConstants.NOTIFICATION_SENT_LEGALFACTS_PATH)
-    public Notification getSentNotificationLegalFacts( 
-            @RequestHeader(name = PnDeliveryRestConstants.USER_ID_HEADER ) String userId,
+    public List<LegalFactsRow> getSentNotificationLegalFacts( 
+            @RequestHeader(name = PnDeliveryRestConstants.PA_ID_HEADER ) String paId,
             @PathVariable( name = "iun") String iun
     );
 
--- src/main/java/it/pagopa/pn_doc/api/PnDeliveryRestController.java
+++ src/main/java/it/pagopa/pn_doc/api/PnDeliveryRestController.java
@@ -1,17 +1,18 @@
 package it.pagopa.pn_doc.api;
 
-import it.pagopa.pn.api.dto.NotificationSearchRow;
-import it.pagopa.pn.api.dto.notification.status.NotificationStatus;
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.pagopa.pn.api.dto.LegalFactsRow;
 import it.pagopa.pn.api.dto.NewNotificationResponse;
+import it.pagopa.pn.api.dto.NotificationSearchRow;
 import it.pagopa.pn.api.dto.notification.Notification;
+import it.pagopa.pn.api.dto.notification.status.NotificationStatus;
 import it.pagopa.pn.api.rest.PnDeliveryRestApi;
-
-import java.time.Instant;
-import java.util.List;
 
 @RestController
 public class PnDeliveryRestController implements PnDeliveryRestApi {
@@ -37,7 +38,7 @@ public class PnDeliveryRestController implements PnDeliveryRestApi {
     }
 
 	@Override
-	public Notification getSentNotificationLegalFacts(String userId, String iun) {
+	public List<LegalFactsRow> getSentNotificationLegalFacts(String paId, String iun) {
 		return null;
 	}
 }
