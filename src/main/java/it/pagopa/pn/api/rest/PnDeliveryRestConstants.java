d23ccc4bb48663d7d371e83756ac2dc5f39579c3
masciale89 <89906206+masciale89@users.noreply.github.com>
2021-10-06 11:52:51 +0200

    unify documents controller and fix legal facts path (#25)

--- src/main/java/it/pagopa/pn/api/rest/PnDeliveryRestConstants.java
+++ src/main/java/it/pagopa/pn/api/rest/PnDeliveryRestConstants.java
@@ -20,7 +20,7 @@ public class PnDeliveryRestConstants {
 
     public static final String NOTIFICATIONS_RECEIVED_PATH = DELIVERY_PATH + "/notifications/received";
     public static final String NOTIFICATION_RECEIVED_PATH = DELIVERY_PATH + "/notifications/received/{iun}";
-    public static final String NOTIFICATION_RECEIVED_LEGALFACTS_PATH = DELIVERY_PATH + "/notifications/received/{iun}/legalfacts/";
+    public static final String NOTIFICATION_RECEIVED_LEGALFACTS_PATH = DELIVERY_PATH + "/notifications/received/{iun}/legalfacts";
     public static final String NOTIFICATION_VIEWED_PATH = DELIVERY_PATH + "/notifications/received/{iun}/documents/{documentIndex}";
 
 }
