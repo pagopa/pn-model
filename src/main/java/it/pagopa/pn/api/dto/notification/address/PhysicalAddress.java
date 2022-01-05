22ba90a23ae31e507f7f9d7aae0ba34e46fc2d59
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-01-05 17:23:56 +0100

    Revert "Test per build CI (modifica sorgenti)"
    This reverts commit a0759644c178300f13dc83fdfb64cadff82219bf.

--- src/main/java/it/pagopa/pn/api/dto/notification/address/PhysicalAddress.java
+++ src/main/java/it/pagopa/pn/api/dto/notification/address/PhysicalAddress.java
@@ -51,7 +51,7 @@ public class PhysicalAddress {
 		standardAddressString.add( recipientDenomination );
 		
 		if ( isNotBlank( at ) ) {
-			standardAddressString.add( at  + "!");
+			standardAddressString.add( at );
 		}
 		
 		if ( isNotBlank( addressDetails ) ) {
