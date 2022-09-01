8ad47cc5bc69282e7c72c6beed30d06b22ff9dda
Simone Rondinella <simone.rondinella@pagopa.it>
2022-09-01 20:14:38 +0200

    PN-1791 Aggiunta modelli per inserimento in queue

--- src/main/java/it/pagopa/pn/api/dto/events/EventType.java
+++ src/main/java/it/pagopa/pn/api/dto/events/EventType.java
@@ -3,12 +3,13 @@ package it.pagopa.pn.api.dto.events;
 public enum EventType implements IEventType {
     NEW_NOTIFICATION( PnDeliveryNewNotificationEvent.class ),
     NOTIFICATION_VIEWED( PnDeliveryNotificationViewedEvent.class ),
+    NOTIFICATION_PAID( PnExtRegistryNotificationPaidEvent.class ),
     SEND_PEC_REQUEST( PnExtChnPecEvent.class ),
     SEND_PEC_RESPONSE( PnExtChnProgressStatusEvent.class ),
     SEND_COURTESY_EMAIL ( PnExtChnEmailEvent.class ),
     SEND_PAPER_REQUEST ( PnExtChnPaperEvent.class ),
     SEND_PAPER_RESPONSE ( PnExtChnProgressStatusEvent.class );
-
+    
     private final Class<?> eventClass;
 
     EventType( Class<?> eventClass) {
