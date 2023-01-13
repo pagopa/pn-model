7d404eef3b10f8223a52c5ee7ddf50395c2c3256
Alessandro Masci <alessandro.masci@pagopa.it>
2023-01-13 12:44:42 +0100

    PN-2188: add PnDeliveryPaymentEvent

---
+++ src/main/java/it/pagopa/pn/api/dto/events/PnDeliveryPaymentEvent.java
@@ -0,0 +1,103 @@
+package it.pagopa.pn.api.dto.events;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder(toBuilder = true)
+@EqualsAndHashCode
+@ToString
+public class PnDeliveryPaymentEvent implements GenericFifoEvent<StandardEventHeader, PnDeliveryPaymentEvent.Payload> {
+
+    private StandardEventHeader header;
+
+    private Payload payload;
+
+    private String messageDeduplicationId;
+
+    private String messageGroupId;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder(toBuilder = true)
+    @EqualsAndHashCode
+    @ToString
+    public static class Payload {
+
+        //@Schema( description = "Identificativo univoco della richiesta" )
+        @NotEmpty
+        private String iun;
+
+        //@Schema( description = "Indice dell'array del destinatario della notifica" )
+        private int recipientIdx;
+
+        //@Schema( description = "Tipo di destinatario PF|PG" )
+        @NotNull
+        private RecipientType recipientType;
+
+        //@Schema( description = "Tipo di pagamento PAGOPA|F24" )
+        @NotNull
+        private PaymentType paymentType;
+
+        //@Schema( description = "Data di pagamento")
+        @NotNull
+        private Instant paymentDate;
+
+        //@Schema( description = "Codice fiscale dell'ente creditore. Obbligatorio in caso PaymentType PAGOPA")
+        private String creditorTaxId;
+
+        //@Schema( description = "Numero della posizione debitoria. Obbligatorio in caso PaymentType PAGOPA")
+        private String noticeCode;
+
+    }
+
+    public enum RecipientType {
+        PF("PF"),
+
+        PG("PG");
+
+        private final String value;
+
+        RecipientType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+
+    public enum PaymentType {
+        PF("PAGOPA"),
+
+        PG("F24");
+
+        private final String value;
+
+        PaymentType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+
+
+}
