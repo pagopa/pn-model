d745f7a5a3ec02e55e953bf428465da6a0a90066
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-02-24 18:14:10 +0100

    PN-3496 pagamenti da delivery

--- src/main/java/it/pagopa/pn/api/dto/events/EventType.java
+++ src/main/java/it/pagopa/pn/api/dto/events/EventType.java
@@ -9,7 +9,7 @@ public enum EventType implements IEventType {
     SEND_PEC_RESPONSE( PnExtChnProgressStatusEvent.class ),
     SEND_COURTESY_EMAIL ( PnExtChnEmailEvent.class ),
     SEND_PAPER_REQUEST ( PnExtChnPaperEvent.class ),
-    SEND_PAPER_RESPONSE ( PnExtChnProgressStatusEvent.class ),
+    SEND_PAPER_RESPONSE ( PnDeliveryPaymentEvent.class ),
     SEND_ONBOARDING_REQUEST (PnOnboardingInstitutionEvent.class),
     MANDATE_ACCEPTED (PnMandateEvent.class),
     MANDATE_REVOKED (PnMandateEvent.class),
